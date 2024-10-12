a4bde4c81ad765cac41d21fa5d2d58b87ec8562b
PhuscBui <duyphucbui522@gmail.com>
2024-10-12 10:52:06 +0700

    Update sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,85 @@
-import React from "react";
+"use client";
+
+import {
+  ClipboardCheck,
+  ClipboardMinus,
+  ClipboardPenLine,
+  LayoutDashboard,
+  ListTodo,
+  Receipt,
+  Search,
+  Settings,
+  UserCheck,
+  UserCog,
+  Users,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface MenuItemProps {
+  icon: React.ReactNode;
+  title: string;
+  path: string;
+}
+
+function MenuItem({ icon, title, path }: MenuItemProps) {
+  const currentPath = usePathname();
+  const isActive = currentPath === path;
+
+  if (isActive) {
+    return (
+      <Link href={path}>
+        <div className="flex items-center p-[10px] space-x-[10px] text-white text-[14px] w-[200px] bg-primary rounded-[5px]">
+          <span>{icon}</span>
+          <span className="leading-6">{title}</span>
+        </div>
+      </Link>
+    );
+  }
+  return (
+    <Link href={path}>
+      <div className="flex items-center p-[10px] space-x-[10px] text-white text-[14px] w-[200px] ">
+        <span>{icon}</span>
+        <span className="leading-6">{title}</span>
+      </div>
+    </Link>
+  );
+}
 
 export default function Sidebar() {
-  return <div className="w-60 h-[900px] bg-slate-800 fixed"></div>;
+  return (
+    <div className="w-60 h-[900px] bg-secondary fixed">
+      <div className="p-5 gap-y-3 border-b border-[#FCFCFC]/[40]">
+        <MenuItem
+          icon={<LayoutDashboard />}
+          title="Dashboard"
+          path="/dashboard"
+        />
+        <MenuItem icon={<ListTodo />} title="Tasks" path="/tasks" />
+        <MenuItem icon={<Search />} title="Rates" path="/rates" />
+        <MenuItem icon={<Receipt />} title="Quotes" path="/quotes" />
+        <MenuItem
+          icon={<ClipboardCheck />}
+          title="Shipments"
+          path="/shipment"
+        />
+      </div>
+      <div className="p-5 gap-y-3 border-b border-[#FCFCFC]/[40]">
+        <MenuItem icon={<UserCog />} title="Employees" path="/employees" />
+        <MenuItem icon={<Users />} title="Clients" path="/clients" />
+        <MenuItem icon={<UserCheck />} title="Providers" path="/providers" />
+      </div>
+      <div className="p-5 gap-y-3 border-b border-[#FCFCFC]/[40]">
+        <MenuItem
+          icon={<ClipboardPenLine />}
+          title="Accounting"
+          path="/accounting"
+        />
+        <MenuItem icon={<ClipboardMinus />} title="Report" path="/report" />
+      </div>
+      <div className="p-5 gap-y-3 ">
+        <MenuItem icon={<Settings />} title="Settings" path="/test" />
+      </div>
+    </div>
+  );
 }
