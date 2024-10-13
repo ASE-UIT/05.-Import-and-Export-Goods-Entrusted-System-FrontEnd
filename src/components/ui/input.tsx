5291d09c42e0ab4f27e4eefbc54c27d20f7e3203
PhuscBui <duyphucbui522@gmail.com>
2024-10-13 08:23:57 +0700

    Minor change

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-[rgb(0,0,0)]/25 dark:border-foreground  bg-transparent px-3 py-1 text-sm shadow-[0_2px_5px_0px_rgba(0,0,0,0.25)] transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-9 w-full rounded-md border border-[rgb(0,0,0)]/25 dark:border-foreground bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
