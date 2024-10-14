f43344c1c361c85daf6814a7b4b18c682f30ad87
PhuscBui <duyphucbui522@gmail.com>
2024-10-14 08:55:43 +0700

    [CNP-76] Develop Dashboard/Reporting Screen

---
+++ src/app/(pages)/dashboard/_components/columns.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+export type TableShipmentTracking = {
+  shipment_id: number;
+  tracking_id: number;
+  shipment_type: string;
+  location: string;
+  status: string;
+};
+
+export const columns: ColumnDef<TableShipmentTracking>[] = [
+  {
+    accessorKey: "shipment_id",
+    header: "Shipment ID",
+  },
+  {
+    accessorKey: "tracking_id",
+    header: "Tracking ID",
+  },
+  {
+    accessorKey: "shipment_type",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Shipment Type
+          <ArrowUpDown className="ml-2 size-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("shipment_type")}</div>,
+  },
+  {
+    accessorKey: "location",
+    header: ({ column }) => {
+      return (
+        <Button
+          className="pl-0"
+          variant="ghost"
+          style={{ backgroundColor: "transparent" }}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Location
+          <ArrowUpDown className="ml-2 size-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div>{row.getValue("location")}</div>,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+];
