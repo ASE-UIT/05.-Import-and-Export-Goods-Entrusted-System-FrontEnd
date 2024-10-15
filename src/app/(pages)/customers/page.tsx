1e04b65a5424b013f1fd2a723bcb10f892638b79
JibrilFlugel <anhkhoi.anhkhoi19@gmail.com>
2024-10-15 21:56:53 +0700

    feature: pagination

--- src/app/(pages)/customers/page.tsx
+++ src/app/(pages)/customers/page.tsx
@@ -16,39 +16,10 @@ import {
 import { useState } from "react";
 import { useReactTable } from "@tanstack/react-table";
 import { DataTableFilter } from "./components/data-table-filter";
+import { customerData } from "./data/customer-data";
 
 export default function CustomerManagementPage() {
   const [currentFilter, setCurrentFilter] = useState<string | null>(null);
-
-  const data: ICustomer[] = [
-    {
-      name: "Nguyen Van A",
-      shortName: "NVA",
-      email: "AnhLong@gmail.com",
-      phone: "0123456789",
-      taxId: "123",
-      address: "HCMC",
-      legalRepName: "AL",
-    },
-    {
-      name: "Tran Hoang B",
-      shortName: "THB",
-      email: "AnhLong@gmail.com",
-      phone: "0123456789",
-      taxId: "123",
-      address: "HCMC",
-      legalRepName: "AL",
-    },
-    {
-      name: "Le Thi C",
-      shortName: "LTC",
-      email: "AnhLong@gmail.com",
-      phone: "0123456789",
-      taxId: "123",
-      address: "HCMC",
-      legalRepName: "AL",
-    },
-  ];
 
   return (
     <div className="flex flex-col p-[28px] w-full h-[calc(100vh-60px)] flex-grow">
@@ -56,7 +27,7 @@ export default function CustomerManagementPage() {
         <div className="flex justify-between items-center">
           <span className="text-3xl font-bold">Customer</span>
         </div>
-        <DataTable columns={columns} data={data} />
+        <DataTable columns={columns} data={customerData} />
       </div>
     </div>
   );
