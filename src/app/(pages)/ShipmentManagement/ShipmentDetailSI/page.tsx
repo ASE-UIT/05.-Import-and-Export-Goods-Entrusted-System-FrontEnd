ea362845738f9354245afe8fd27fa3f142332995
latteinthecloud <22520644@gm.uit.edu.vn>
2024-10-18 10:32:04 +0700

    Shipment Screen

---
+++ src/app/(pages)/ShipmentManagement/ShipmentDetailSI/page.tsx
@@ -0,0 +1,11 @@
+
+export default function ShipmentDetailSI(){
+    return(
+        <div className="h-[946px] p-6 flex-col justify-start items-center gap-3 inline-flex">
+      <div className="self-stretch justify-start items-center inline-flex">
+        <div className="text-black text-3xl font-bold font-['Inter']">
+          Shipment
+        </div>
+        </div>
+    )
+  }
