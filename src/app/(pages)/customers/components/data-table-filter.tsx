4e05d3b7de842d05e15b8ce64c87f1898ffc1ad4
JibrilFlugel <anhkhoi.anhkhoi19@gmail.com>
2024-10-18 18:09:16 +0700

    Minor change

--- src/app/(pages)/customers/components/data-table-filter.tsx
+++ src/app/(pages)/customers/components/data-table-filter.tsx
@@ -26,8 +26,8 @@ export function DataTableFilter<TData>({
   const [currentFilter, setCurrentFilter] = useState<string>(columnsTitle[0]);
   console.log(columnsTitle);
   return (
-    <div className="flex gap-[5px]">
-      <div className="w-[300px]">
+    <div className="flex gap-[5px] items-center">
+      <div className="w-[300px] ">
         <div className="relative">
           <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
           <Input
@@ -36,7 +36,7 @@ export function DataTableFilter<TData>({
             onChange={(event) =>
               table.getColumn(currentFilter)?.setFilterValue(event.target.value)
             }
-            className="pl-8"
+            className="pl-8 h-2.25"
           />
         </div>
       </div>
