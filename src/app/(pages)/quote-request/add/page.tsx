27e850fafbc098abfba35b87726376a185536a5f
khanhqueng <quangkhanh288123@gmail.com>
2024-10-19 16:28:07 +0700

    update add quote request screen

--- src/app/(pages)/quote-request/add/page.tsx
+++ src/app/(pages)/quote-request/add/page.tsx
@@ -2,14 +2,14 @@
 
 import QuoteRequestAddForm from "./components/add-form";
 
-export default function QuoteRequestAddPage(){
+export default function QuoteRequestAddPage() {
   return (
     <div className="flex flex-col p-[24px] w-full h-full flex-grow ">
       <div className="flex flex-col w-full gap-[20px] ">
         <div className="flex justify-between items-center ">
           <span className="text-2xl font-bold">Create New Quote Request</span>
         </div>
-        <QuoteRequestAddForm/>
+        <QuoteRequestAddForm />
       </div>
     </div>
   );
