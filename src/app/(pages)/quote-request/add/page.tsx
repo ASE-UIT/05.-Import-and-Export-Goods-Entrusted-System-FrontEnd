3ac3eff5b1f547c171b7a082b2e7a44e171bddbf
khanhqueng <quangkhanh288123@gmail.com>
2024-10-20 18:06:43 +0700

    Fix overflow

--- src/app/(pages)/quote-request/add/page.tsx
+++ src/app/(pages)/quote-request/add/page.tsx
@@ -4,9 +4,9 @@ import QuoteRequestAddForm from "./components/add-form";
 
 export default function QuoteRequestAddPage(){
   return (
-    <div className="flex flex-col p-[28px] w-full h-[calc(100vh-60px)] flex-grow">
-      <div className="flex flex-col w-full gap-[20px]">
-        <div className="flex justify-between items-center">
+    <div className="flex flex-col p-[24px] w-full h-full flex-grow ">
+      <div className="flex flex-col w-full gap-[20px] ">
+        <div className="flex justify-between items-center ">
           <span className="text-2xl font-bold">Create New Quote Request</span>
         </div>
         <QuoteRequestAddForm/>
