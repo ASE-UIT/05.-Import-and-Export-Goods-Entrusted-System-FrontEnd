d92b13c23d0751e03c7b9cc1b60098cf6f80b4e7
latteinthecloud <22520644@gm.uit.edu.vn>
2024-10-22 11:18:38 +0700

    fix: screen service

---
+++ src/app/(pages)/service/update/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { z } from "zod";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select"; // Nhập Select
+import { useParams } from "next/navigation";
+
+const formSchema = z.object({
+  name: z.string(),
+  shortname: z.string(),
+  fee: z.string(),
+});
+
+export default function UpdateService() {
+  const { id: customerId } = useParams<{ id: string }>();
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex flex-col items-center p-[24px] w-full">
+      <div className="flex w-full justify-between items-end">
+        <span className="text-3xl font-bold">Update Service</span>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          encType="multipart/form-data"
+        >
+          <div className="flex flex-col items-center w-[600px] gap-4 py-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="font-bold">Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="shortname"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="font-bold">Short name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Short name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="fee"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel className="font-bold">Fee</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fee" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="w-1/2 flex gap-2.5">
+              <Link
+                href="/service"
+                className="w-1/2 h-14 text-lg bg-white text-black"
+              >
+                <Button
+                  className="w-full h-10 text-lg bg-white text-black"
+                  type="button"
+                >
+                  Cancel
+                </Button>
+              </Link>
+              <Button className="w-1/2 h-10 text-lg" type="submit">
+                Save
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+}
