d6b29cfe9a0ca7e6fc38597aaba0e56e5d88c774
latteinthecloud <22520644@gm.uit.edu.vn>
2024-10-23 19:33:24 +0700

    chang ui

--- src/app/(pages)/shipment/add/page.tsx
+++ src/app/(pages)/shipment/add/page.tsx
@@ -24,12 +24,12 @@ import {
 } from "@/components/ui/select"; // Nhập Select
 
 const formSchema = z.object({
-  name: z.string(),
-  shortname: z.string(),
-  fee: z.string(),
+  id: z.string(),
+  type: z.string(),
+  contractID: z.string(),
 });
 
-export default function UpdateService() {
+export default function AddShipment() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
@@ -41,7 +41,7 @@ export default function UpdateService() {
   return (
     <div className="flex flex-col items-center p-[24px] w-full">
       <div className="flex w-full justify-between items-end">
-        <span className="text-3xl font-bold">Update Service</span>
+        <span className="text-3xl font-bold">Add Shipment</span>
       </div>
       <Form {...form}>
         <form
@@ -51,12 +51,12 @@ export default function UpdateService() {
           <div className="flex flex-col items-center w-[600px] gap-4 py-4">
             <FormField
               control={form.control}
-              name="name"
+              name="id"
               render={({ field }) => (
                 <FormItem className="w-full">
                   <FormLabel className="font-bold">Name</FormLabel>
                   <FormControl>
-                    <Input placeholder="Name" {...field} />
+                    <Input placeholder="Type an ID" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -65,25 +65,40 @@ export default function UpdateService() {
 
             <FormField
               control={form.control}
-              name="shortname"
+              name="type"
               render={({ field }) => (
                 <FormItem className="w-full">
-                  <FormLabel className="font-bold">Short name</FormLabel>
+                  <FormLabel className="font-bold">Type</FormLabel>
                   <FormControl>
-                    <Input placeholder="Short name" {...field} />
+                    <Select
+                      onValueChange={field.onChange} // Handles changes in selected value
+                      value={field.value} // Value from the form field
+                    >
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select type" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="Air Freight">Air Freight</SelectItem>
+                        <SelectItem value="Land Freight">
+                          Land Freight
+                        </SelectItem>
+                        <SelectItem value="Sea Freight">Sea Freight</SelectItem>
+                      </SelectContent>
+                    </Select>
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
+
             <FormField
               control={form.control}
-              name="fee"
+              name="contractID"
               render={({ field }) => (
                 <FormItem className="w-full">
                   <FormLabel className="font-bold">Fee</FormLabel>
                   <FormControl>
-                    <Input placeholder="Fee" {...field} />
+                    <Input placeholder="Type a contract ID" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -96,7 +111,8 @@ export default function UpdateService() {
                 className="w-1/2 h-14 text-lg bg-white text-black"
               >
                 <Button
-                  className="w-full h-10 text-lg bg-white text-black"
+                  variant={"outline"}
+                  className="w-full h-10 text-lg"
                   type="button"
                 >
                   Cancel
