5584450558480ac095a08f7657c51da601970aff
latteinthecloud <22520644@gm.uit.edu.vn>
2024-10-24 17:30:36 +0700

    fix: button

--- src/app/(pages)/service/update/[id]/page.tsx
+++ src/app/(pages)/service/update/[id]/page.tsx
@@ -91,10 +91,7 @@ export default function UpdateService() {
             />
 
             <div className="w-1/2 flex gap-2.5">
-              <Link
-                href="/service"
-                className="w-1/2 h-14 text-lg bg-white text-black"
-              >
+              <Link href="/service" className="w-1/2 h-14 text-lg">
                 <Button
                   variant={"outline"}
                   className="w-full h-10 text-lg"
