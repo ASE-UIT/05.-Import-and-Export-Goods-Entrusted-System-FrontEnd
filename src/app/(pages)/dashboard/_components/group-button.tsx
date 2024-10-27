8bc976e0ef128a9659121050213cd9295ff01090
PhuscBui <duyphucbui522@gmail.com>
2024-10-27 21:36:04 +0700

    Minor change

--- src/app/(pages)/dashboard/_components/group-button.tsx
+++ src/app/(pages)/dashboard/_components/group-button.tsx
@@ -4,35 +4,44 @@ import {
   UserCog,
   Users,
 } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function GroupButton() {
   return (
     <div className="flex flex-col space-y-2">
-      <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
-        <div className="flex gap-3 items-center text-2xl dark:text-black">
-          <ClipboardPenLine />
-          New Quote
-        </div>
-      </button>
-      <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
-        <div className="flex gap-3 items-center text-2xl dark:text-black">
-          <UserCog />
-          Add Employee
-        </div>
-      </button>
-      <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
-        <div className="flex gap-3 items-center text-2xl dark:text-black">
-          <Users />
-          Add Client
-        </div>
-      </button>
-      <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
-        <div className="flex gap-3 items-center text-2xl dark:text-black">
-          <SquareUserRound />
-          Add Vendor
-        </div>
-      </button>
+      <Link href="/quotation/add">
+        <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
+          <div className="flex gap-3 items-center text-2xl dark:text-black">
+            <ClipboardPenLine />
+            New Quote
+          </div>
+        </button>
+      </Link>
+      <Link href="/employees/add">
+        <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
+          <div className="flex gap-3 items-center text-2xl dark:text-black">
+            <UserCog />
+            Add Employee
+          </div>
+        </button>
+      </Link>
+      <Link href="/customers/add">
+        <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
+          <div className="flex gap-3 items-center text-2xl dark:text-black">
+            <Users />
+            Add Customer
+          </div>
+        </button>
+      </Link>
+      <Link href="/provider/add">
+        <button className="p-5 w-[280px] bg-[#DDD7D7] rounded-lg hover:bg-[#9f9b9b] hover:text-white">
+          <div className="flex gap-3 items-center text-2xl dark:text-black">
+            <SquareUserRound />
+            Add Provider
+          </div>
+        </button>
+      </Link>
     </div>
   );
 }
