e8cbed3b3fdd5a0ce9f49f3339b8ffe2c9d88955
khanhqueng <quangkhanh288123@gmail.com>
2024-11-11 01:04:31 +0700

    intergrate API

--- src/app/(pages)/quote-request/page.tsx
+++ src/app/(pages)/quote-request/page.tsx
@@ -3,6 +3,8 @@ import { columns } from "./components/columns";
 import { DataTable } from "./components/table";
 import useQuoteRequest from "@/hooks/use-quote-request";
 import mapToQuoteRequest from "@/schema/quote-request.schema";
+import useQuoteRequest from "@/hooks/use-quote-request";
+import mapToQuoteRequest from "@/schema/quote-request.schema";
 
 export default function QuoteRequestManagementPage() {
   const { data, isLoading, error } = useQuoteRequest.useGetQuoteRequest();
@@ -13,13 +15,14 @@ export default function QuoteRequestManagementPage() {
   if (error) {
     return <div>Error loading quote requests: {error.message}</div>;
   }
-  const transformedData = data ? mapToQuoteRequest(data) : [];
+  const transformedData = data? [mapToQuoteRequest(data)] : [];
   return (
     <div className="flex flex-col p-[28px] w-full h-[calc(100vh-60px)] flex-grow">
       <div className="flex flex-col w-full gap-[20px]">
         <div className="flex justify-between items-center">
           <span className="text-3xl font-bold">Quote Request Management</span>
         </div>
+        <DataTable columns={columns} data={transformedData} />
         <DataTable columns={columns} data={transformedData} />
       </div>
     </div>
--- src/hooks/use-quote-request.ts
+++ src/hooks/use-quote-request.ts
@@ -1,11 +1,11 @@
 import quoteRequestAction from "@/apis/quote-request.api";
-import { CreateQuoteRequestType, UpdateQuoteRequestType } from "@/schema/quote-request.schema";
+import { CreateQuoteRequestType } from "@/schema/quote-request.schema";
 import { ErrorType } from "@/types/error.type";
-import { useMutation, UseMutationResult, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import { toast } from "./use-toast";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Console } from "console";
+
 const useQuoteRequest ={
-    useCreateQuoteRequest(router: ReturnType<typeof useRouter>) {
+    useCreateQuoteRequest() {
         const queryClient = useQueryClient();
         return useMutation({
         mutationFn: (createQuoteRequestBody: CreateQuoteRequestType) =>
@@ -14,57 +14,13 @@ const useQuoteRequest ={
             queryClient.invalidateQueries({
             queryKey: ["quote-request"],
             });
-            toast({
-                title: "Create success",
-                description: "Quote request created successfully",
-                duration: 10000
-            });
-            router.push("/quote-request");
         },
         onError: (error: ErrorType) => {
             console.error("Error during create:", error);
-            toast({
-            title: "Create failed",
-            description: "An error occurred while creating quote request",
-            variant: "destructive",
-            duration: 5000,
-          });
             throw error;
         },
         });
     },
-
-    useUpdateQuoteRequest(
-    id: string,
-    router: ReturnType<typeof useRouter>
-  ): UseMutationResult<unknown, ErrorType, Partial<UpdateQuoteRequestType>> {
-    const queryClient = useQueryClient();
-    return useMutation({
-      mutationFn: (updateQuoteRequestBody: Partial<UpdateQuoteRequestType>) =>
-        quoteRequestAction.updateQuoteRequest(id, updateQuoteRequestBody),
-        onSuccess: () => {
-            queryClient.invalidateQueries({
-            queryKey: ["quote-request"],
-            });
-            toast({
-                title: "Update success",
-                description: "Quote request updated successfully",
-                duration: 10000
-            });
-            router.push("/quote-request");
-        },
-        onError: (error: ErrorType) => {
-            console.error("Error during update:", error);
-            toast({
-            title: "Update failed",
-            description: "An error occurred while updating quote request",
-            variant: "destructive",
-            duration: 5000,
-          });
-            throw error;
-        },
-    });
-  },
 
     useGetQuoteRequest() {
         return useQuery({
@@ -72,69 +28,10 @@ const useQuoteRequest ={
         queryFn: async () => {
             try {
             const result = await quoteRequestAction.getQuoteRequest();
+            console.log("Hello");;
             return result;
             } catch (error) {
             console.error("Error during get quote request:", error);
-            throw error;
-            }
-        },
-        retry: 0,
-        });
-    },
-    useGetQuoteRequestDetail(quoteRequestId: string) {
-        return useQuery({
-        queryKey: ["quote-request-detail", quoteRequestId],
-        queryFn: async () => {
-            try {
-            const result = await quoteRequestAction.getQuoteRequestDetails(quoteRequestId);
-            return result;
-            } catch (error) {
-            console.error("Error during get quote request:", error);
-            throw error;
-            }
-        },
-        retry: 0,
-        });
-    },
-    useGetFullQuoteRequestDetail(quoteRequestId: string) {
-        return useQuery({
-        queryKey: ["quote-request-full-detail", quoteRequestId],
-        queryFn: async () => {
-            try {
-            const result = await quoteRequestAction.getFullQuoteRequestDetails(quoteRequestId);
-            return result;
-            } catch (error) {
-            console.error("Error during get quote request:", error);
-            throw error;
-            }
-        },
-        retry: 0,
-        });
-    },
-    useGetPackageDetail(quoteReqDetailsId: string) {
-        return useQuery({
-        queryKey: ["package-detail", quoteReqDetailsId],
-        queryFn: async () => {
-            try {
-            const result = await quoteRequestAction.getPackageDetails(quoteReqDetailsId);
-            return result;
-            } catch (error) {
-            console.error("Error during get package details:", error);
-            throw error;
-            }
-        },
-        retry: 0,
-        });
-    },
-    useGetCustomerInfo() {
-        return useQuery({
-        queryKey: ["customers"],
-        queryFn: async () => {
-            try {
-            const result = await quoteRequestAction.getCustomerInfo();
-            return result;
-            } catch (error) {
-            console.error("Error during get customer info:", error);
             throw error;
             }
         },
