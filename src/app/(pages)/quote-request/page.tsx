f907659f320bd6a6976775acdb6d9f12ef973121
khanhqueng <quangkhanh288123@gmail.com>
2024-11-11 01:04:31 +0700

    intergrate API

--- src/app/(pages)/quote-request/page.tsx
+++ src/app/(pages)/quote-request/page.tsx
@@ -13,7 +13,7 @@ export default function QuoteRequestManagementPage() {
   if (error) {
     return <div>Error loading quote requests: {error.message}</div>;
   }
-  const transformedData = data ? mapToQuoteRequest(data) : [];
+  const transformedData = data ? [mapToQuoteRequest(data)] : [];
   return (
     <div className="flex flex-col p-[28px] w-full h-[calc(100vh-60px)] flex-grow">
       <div className="flex flex-col w-full gap-[20px]">
--- src/schema/quote-request.schema.ts
+++ src/schema/quote-request.schema.ts
@@ -1,82 +1,40 @@
 import { z } from "zod";
 export const createQuoteRequestBody = z
   .object({
-    requestDate: z.string(),
-    customerId: z.string(),
-    origin: z.string(),
-    destination: z.string(),
-    shipmentReadyDate: z.string(),
-    shipmentDeadline: z.string(),
-    cargoInsurance: z.boolean(),
-    packageType: z.string(),
-    weight: z.number(),
-    length: z.number(),
-    width: z.number(),
-    height: z.number(),
-  });
+    username: z.string().min(1),
+    password: z
+      .string()
+      .min(6, "Password must have at least 6 characters.")
+      .regex(/[A-Z]/, "Password must contain at least 1 capital letter.")
+      .regex(/[a-z]/, "Password must contain at least 1 lower case letter.")
+      .regex(/[0-9]/, "Password must contain at least 1 number.")
+      .regex(
+        /[#?!@$%^&*-]/,
+        "Password must contain at least 1 special character."
+      ),
+  })
+  .strict();
 
-export const getQuoteRequest = z.array(
-    z.object({
-        id: z.string().uuid(),
-        requestDate: z.date(),
-        status: z.string(),
-        customerId: z.string().uuid(),
-        createdAt: z.date(),
-        updatedAt: z.date()
-    })
-);
-export const getQuoteRequestDetails =  z.object({
-        id: z.string().uuid(),
-        origin: z.string(),
-        destination: z.string(),
-        shipmentReadyDate: z.date(),
-        shipmentDeadline: z.date(),
-        cargoInsurance: z.boolean(),
-        quoteReqId: z.string().uuid(),
-        createdAt: z.date(),
-        updatedAt:  z.date()
-    });
-export const getPackageDetails =  z.object({
-        id: z.string().uuid(),
-        packageType: z.string(),
-        weight: z.number(),
-        length: z.number(),
-        width: z.number(),
-        height: z.number(),
-        detailId: z.string().uuid(),
-        createdAt: z.date(),
-        updatedAt:  z.date()
-    });
-export const getCustomerInfo =  z.object({
-        message: z.string(),
-        data: z.array(
-          z.object({
-            id: z.string(),
-            name: z.string(),
-            shortName: z.string(),
-            email: z.string(),
-            phone: z.string(),
-            address: z.string(),
-            taxId: z.string(),
-            legalRepId: z.string()
-          })
-        ),
-       
-    });
-function mapToQuoteRequest(data: z.infer<typeof getQuoteRequest>): QuoteRequest[] {
-  return data.map((data) => ({
-    quote_request_id: data.id,                
-    customer_id: data.customerId,             
-    request_date: new Date(data.requestDate).toDateString(),     
+export const getQuoteRequest = z.object({
+    id: z.string().uuid(),
+    requestDate: z.date(),
+    status: z.string(),
+    customerId: z.string().uuid(),
+    createdAt: z.date(),
+    updatedAt: z.date()
+});
+function mapToQuoteRequest(data: z.infer<typeof getQuoteRequest>): QuoteRequest {
+  return {
+    quote_request_id: parseInt(data.id),               // Assuming `id` is a stringified number
+    customer_id: parseInt(data.customerId),             // Assuming `customerId` is a stringified number
+    request_date: data.requestDate.toDateString(),       // Converting Date to string
     status: data.status,
-    create_at: new Date(data.createdAt).toDateString(),          
-    update_at: new Date(data.updatedAt).toDateString()           
-  }));
+    create_at: data.createdAt.toDateString(),            // Converting Date to string
+    update_at: data.updatedAt.toDateString()             // Converting Date to string
+  };
 }
 
 export type GetQuoteRequestType = z.TypeOf<typeof getQuoteRequest>;
-export type GetQuoteRequestDetailsType = z.TypeOf<typeof getQuoteRequestDetails>;
+
 export type CreateQuoteRequestType = z.TypeOf<typeof createQuoteRequestBody>;
-export type GetPackageDetails= z.TypeOf<typeof getPackageDetails>;
-export type GetCustomerInfo= z.TypeOf<typeof getCustomerInfo>;
 export default mapToQuoteRequest;
