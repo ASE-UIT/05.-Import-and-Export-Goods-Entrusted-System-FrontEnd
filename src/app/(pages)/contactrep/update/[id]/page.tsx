674197029bb02f070397ead39634f06a4767c4a2
Tkhangds <gunnyvippro111@gmail.com>
2024-11-23 23:18:07 +0700

    feat: Refactor contact representative and provider API methods to accept ID parameters; update related hooks and forms for fetching and updating data

--- src/app/(pages)/contactrep/update/[id]/page.tsx
+++ src/app/(pages)/contactrep/update/[id]/page.tsx
@@ -2,10 +2,11 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { z } from "zod";
 import Link from "next/link";
-
+import { useContactRep } from "@/hooks/use-contactRep";
+import { contactRepSchema } from "@/schema/contactRep.schema";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -30,18 +31,55 @@ const formSchema = z.object({
 export default function UpdateContactrep() {
   const { id: contactrepId } = useParams<{ id: string }>();
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { useGetContactRepById, useUpdateContactRep } = useContactRep();
+
+  const updateMutation = useUpdateContactRep();
+
+  const {
+    data: contactrep,
+    error,
+    isPending,
+  } = useGetContactRepById(contactrepId);
+
+  const form = useForm<z.infer<typeof contactRepSchema>>({
+    resolver: zodResolver(contactRepSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  useEffect(() => {
+    if (contactrep) {
+      console.log(contactrep);
+      if (contactrep.data) {
+        const contactRepData = contactrep.data[0];
+
+        form.reset({
+          name: contactRepData.name,
+          email: contactRepData.email,
+          phone: contactRepData.phone,
+        });
+      }
+    }
+  }, [contactrep, form]);
+
+  function onSubmit(values: z.infer<typeof contactRepSchema>) {
+    try {
+      updateMutation.mutate({
+        id: contactrepId,
+        data: values,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
     <div className="flex flex-col items-center p-[24px] w-full">
       <div className="flex w-full justify-between items-end">
-        <span className="text-3xl font-bold">Update Contactrep</span>
+        <span className="text-3xl font-bold">Update ContactRep</span>
       </div>
       <Form {...form}>
         <form
