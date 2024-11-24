5d836057074379c537a49f6185fde7c3e26d1a91
Tkhangds <gunnyvippro111@gmail.com>
2024-11-24 09:38:15 +0700

    feat: Add Zod schemas and update API methods to use typed data for contact representatives and providers

--- src/schema/contactRep.schema.ts
+++ src/schema/contactRep.schema.ts
@@ -1,7 +1,12 @@
-import { z } from "zod";
+import z from "zod";
 
 export const contactRepSchema = z.object({
   name: z.string().min(1, "Name cannot be empty"),
   email: z.string().email(),
   phone: z.string().min(1, "Phone Number cannot be empty"),
 });
+
+export type createContactRepData = z.infer<typeof contactRepSchema>;
+export type updateContactRepData = z.infer<
+  ReturnType<typeof contactRepSchema.partial>
+>;
