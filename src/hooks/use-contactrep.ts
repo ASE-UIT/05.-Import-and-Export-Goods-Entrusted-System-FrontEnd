672cbd8c952e5f91e3dfd04ecbbefadc92a7afb6
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-12-04 00:39:05 +0700

    ddddddd

---
+++ src/hooks/use-contactrep.ts
@@ -0,0 +1,55 @@
+
+import contactRepAction from "@/apis/contactrep.api";
+import { ContactRepBodyType } from "@/schema/contactRep.schema";
+import { ErrorType } from "@/types/error.type";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+const useContactRep = {
+    useGetContactRep() {
+        return useQuery({
+            queryKey: ["contact-rep"],
+            queryFn: async () => {
+                try {
+                    const result = await contactRepAction.getContactRep();
+                    return result.data;
+                } catch (error) {
+                    throw error;
+                }
+            },
+            retry: 0,
+        });
+    },
+
+    useCreateContactRep() {
+        const queryClient = useQueryClient();
+        return useMutation({
+            mutationFn: (contactRepDetails: ContactRepBodyType) =>
+                contactRepAction.createContactRep(contactRepDetails),
+            onSuccess: () => {
+                queryClient.invalidateQueries({
+                    queryKey: ["contact-rep"],
+                });
+            },
+            onError: (error: ErrorType) => {
+                throw error;
+            },
+        });
+    },
+
+    useUpdateContactRep() {
+        const queryClient = useQueryClient();
+        return useMutation({
+            mutationFn: (contactRepDetails: ContactRep) =>
+                contactRepAction.updateContractRep(contactRepDetails),
+            onSuccess: (data) => {
+                queryClient.invalidateQueries({
+                    queryKey: ["contact-rep", data.id],
+                });
+            },
+            onError: (error: ErrorType) => {
+                throw error;
+            },
+        });
+    },
+};
+export default useContactRep;
