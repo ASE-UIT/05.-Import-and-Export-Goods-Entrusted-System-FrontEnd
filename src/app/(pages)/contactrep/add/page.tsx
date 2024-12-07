cfc0fce398e2c5aa6bb1374dbfbfc67c44778fb7
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-12-07 11:09:17 +0700

    add contact rep

--- src/app/(pages)/contactrep/add/page.tsx
+++ src/app/(pages)/contactrep/add/page.tsx
@@ -1,9 +1,131 @@
+// "use client";
+
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+// import Link from "next/link";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { contactRepSchema } from "@/schema/contactRep.schema";
+// import { useContactRep } from "@/hooks/use-contactRep";
+// import { useRouter } from "next/navigation";
+
+// export default function AddContactrep() {
+//   const router = useRouter();
+
+//   const { useCreateContactRep } = useContactRep();
+//   const createContactRep = useCreateContactRep();
+
+//   const form = useForm<z.infer<typeof contactRepSchema>>({
+//     resolver: zodResolver(contactRepSchema),
+//   });
+
+//   function onSubmit(values: z.infer<typeof contactRepSchema>) {
+//     try {
+//       createContactRep.mutate(values, {
+//         onSuccess: () => {
+//           router.push("/contactrep");
+//         },
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+
+//   return (
+//     <div className="flex flex-col items-center p-[24px] w-full">
+//       <div className="flex w-full justify-between items-end">
+//         <span className="text-3xl font-bold">Add Contactrep</span>
+//       </div>
+//       <Form {...form}>
+//         <form
+//           onSubmit={form.handleSubmit(onSubmit)}
+//           encType="multipart/form-data"
+//         >
+//           <div className="flex flex-col items-center w-[600px] gap-4 py-4">
+//             <FormField
+//               control={form.control}
+//               name="name"
+//               render={({ field }) => (
+//                 <FormItem className="w-full">
+//                   <FormLabel className="font-bold">Name</FormLabel>
+//                   <FormControl>
+//                     <Input placeholder="Name" {...field} />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//             <FormField
+//               control={form.control}
+//               name="email"
+//               render={({ field }) => (
+//                 <FormItem className="w-full">
+//                   <FormLabel className="font-bold">Email</FormLabel>
+//                   <FormControl>
+//                     <Input type="email" placeholder="Email" {...field} />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+//             <FormField
+//               control={form.control}
+//               name="phone"
+//               render={({ field }) => (
+//                 <FormItem className="w-full">
+//                   <FormLabel className="font-bold">Phone</FormLabel>
+//                   <FormControl>
+//                     <Input type="tel" placeholder="Phone" {...field} />
+//                   </FormControl>
+//                   <FormMessage />
+//                 </FormItem>
+//               )}
+//             />
+
+//             <div className="w-1/2 flex gap-2.5">
+//               <Link
+//                 href="/contactrep"
+//                 className="w-1/2 h-14 text-lg bg-white text-black"
+//               >
+//                 <Button
+//                   className="w-full h-10  text-lg"
+//                   variant={"outline"}
+//                   type="button"
+//                 >
+//                   Cancel
+//                 </Button>
+//               </Link>
+//               <Button
+//                 className="w-1/2 h-10 text-lg"
+//                 variant={"default"}
+//                 type="submit"
+//               >
+//                 Save
+//               </Button>
+//             </div>
+//           </div>
+//         </form>
+//       </Form>
+//     </div>
+//   );
+// }
+
+
+
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -14,30 +136,57 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { contactRepSchema } from "@/schema/contactRep.schema";
-import { useContactRep } from "@/hooks/use-contactRep";
-import { useRouter } from "next/navigation";
+
+import { ContactRepBody, ContactRepBodyType } from "@/schema/contactRep.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import useContactRep from "@/hooks/use-contactRep";
+import { ErrorType } from "@/types/error.type";
 
 export default function AddContactrep() {
-  const router = useRouter();
-
-  const { useCreateContactRep } = useContactRep();
-  const createContactRep = useCreateContactRep();
-
-  const form = useForm<z.infer<typeof contactRepSchema>>({
-    resolver: zodResolver(contactRepSchema),
+  const form = useForm<ContactRepBodyType>({
+    resolver: zodResolver(ContactRepBody),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
   });
 
-  function onSubmit(values: z.infer<typeof contactRepSchema>) {
-    try {
-      createContactRep.mutate(values, {
-        onSuccess: () => {
-          router.push("/contactrep");
-        },
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  const router = useRouter();
+
+  const { mutate: createContactRep, isPending } =
+    useContactRep.useCreateContactRep();
+
+  function onSubmit(values: ContactRepBodyType) {
+    console.log(values);
+    if (isPending) return;
+
+    createContactRep(values, {
+      onSuccess() {
+        router.push("/contactrep");
+      },
+      onError: (error) => {
+        console.log(" ~ onSubmit ~ error:", error);
+        switch ((error as ErrorType).statusCode) {
+          case 409: {
+            if (!error.errors?.length) return;
+
+            error.errors.forEach((err) => {
+              const filed = err.field as keyof ContactRepBodyType;
+              form.setError(filed, {
+                message: err.message,
+              });
+            });
+            break;
+          }
+          default:
+            console.error("Unknown error", error);
+            break;
+        }
+      },
+    });
   }
 
   return (
@@ -49,6 +198,7 @@ export default function AddContactrep() {
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           encType="multipart/form-data"
+          noValidate
         >
           <div className="flex flex-col items-center w-[600px] gap-4 py-4">
             <FormField
@@ -56,7 +206,9 @@ export default function AddContactrep() {
               name="name"
               render={({ field }) => (
                 <FormItem className="w-full">
-                  <FormLabel className="font-bold">Name</FormLabel>
+                  <FormLabel className="font-bold">
+                    Name
+                  </FormLabel>
                   <FormControl>
                     <Input placeholder="Name" {...field} />
                   </FormControl>
@@ -69,9 +221,15 @@ export default function AddContactrep() {
               name="email"
               render={({ field }) => (
                 <FormItem className="w-full">
-                  <FormLabel className="font-bold">Email</FormLabel>
+                  <FormLabel className="font-bold">
+                    Email
+                  </FormLabel>
                   <FormControl>
-                    <Input type="email" placeholder="Email" {...field} />
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -82,9 +240,15 @@ export default function AddContactrep() {
               name="phone"
               render={({ field }) => (
                 <FormItem className="w-full">
-                  <FormLabel className="font-bold">Phone</FormLabel>
+                  <FormLabel className="font-bold">
+                    Phone
+                  </FormLabel>
                   <FormControl>
-                    <Input type="tel" placeholder="Phone" {...field} />
+                    <Input
+                      type="tel"
+                      placeholder="Phone"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -108,6 +272,7 @@ export default function AddContactrep() {
                 className="w-1/2 h-10 text-lg"
                 variant={"default"}
                 type="submit"
+                disabled={isPending}
               >
                 Save
               </Button>
--- src/app/(pages)/contactrep/page.tsx
+++ src/app/(pages)/contactrep/page.tsx
@@ -1,27 +1,79 @@
+// "use client";
+
+// import { DataTable } from "@/app/(pages)/contactrep/components/data-table";
+// import { columns } from "./components/columns";
+// import { contactrepData as sampleData } from "./data/sample-data";
+// import { useContactRep } from "@/hooks/use-contactRep";
+
+// export default function ContactrepManagement() {
+//   const { useGetAllContactRep } = useContactRep();
+
+//   const { data: contactRepData, error, isPending } = useGetAllContactRep();
+
+//   return (
+//     <div className="flex flex-col p-[24px] w-full">
+//       <div className="flex flex-col w-full gap-[20px]">
+//         <div className="flex justify-between items-center">
+//           <span className="text-3xl font-bold">ContactRep Management</span>
+//         </div>
+//         <DataTable
+//           columns={columns}
+//           data={contactRepData?.data ?? sampleData}
+//           error={error}
+//           isPending={isPending}
+//         />
+//       </div>
+//     </div>
+//   );
+// }
 "use client";
 
 import { DataTable } from "@/app/(pages)/contactrep/components/data-table";
+import { Loader2, TriangleAlert } from "lucide-react";
 import { columns } from "./components/columns";
-import { contactrepData as sampleData } from "./data/sample-data";
-import { useContactRep } from "@/hooks/use-contactRep";
+
+import useContactRep from "@/hooks/use-contactRep";
 
 export default function ContactrepManagement() {
-  const { useGetAllContactRep } = useContactRep();
+  const {
+    data: contactrepData,
+    isPending,
+    isError,
+  } = useContactRep.useGetContactRep();
 
-  const { data: contactRepData, error, isPending } = useGetAllContactRep();
+  if (isPending) {
+    return (
+      <div className="absolute top-1/2 -translate-y-1/2 flex items-center justify-center">
+        <div>
+          <Loader2 className="size-10 animate-spin text-muted-foreground mx-auto" />
+          <h2 className="text-lg">Loading Contactrep...</h2>
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="absolute top-1/2 -translate-y-1/2 flex items-center justify-center">
+        <div>
+          <TriangleAlert className="size-10 text-destructive mx-auto" />
+          <h2 className="text-lg text-destructive">
+            Error loading contactrep
+          </h2>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col p-[24px] w-full">
       <div className="flex flex-col w-full gap-[20px]">
         <div className="flex justify-between items-center">
-          <span className="text-3xl font-bold">ContactRep Management</span>
+          <span className="text-3xl font-bold">
+            Contactrep Management
+          </span>
         </div>
-        <DataTable
-          columns={columns}
-          data={contactRepData?.data ?? sampleData}
-          error={error}
-          isPending={isPending}
-        />
+        <DataTable columns={columns} data={contactrepData} />
       </div>
     </div>
   );
