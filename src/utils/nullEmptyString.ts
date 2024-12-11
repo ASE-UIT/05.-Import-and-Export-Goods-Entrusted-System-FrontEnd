f34b9b444bc1496c23d3ac1dd934015e5487d5e9
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-12-11 21:42:47 +0700

    fs

--- src/utils/nullEmptyString.ts
+++ src/utils/nullEmptyString.ts
@@ -1,8 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const nullEmptyString = <T extends Record<string, any>>(obj: T): T => {
     const result = {} as T;
 
     for (const key in obj) {
-        if (obj[key] === "") {
+        if (obj[key] === '') {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             result[key] = null as any;
         } else {
             result[key] = obj[key];
