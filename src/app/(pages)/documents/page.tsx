2e220f223f257eca0f7772a1aaa2c2196d7093f1
lvakhoa <22520674@gm.uit.edu.vn>
2024-12-20 15:40:50 +0700

    Create component for bill of landing sea and air doc

---
+++ src/app/(pages)/documents/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BillOfLanding from './components/bill-of-landing/bill-of-landing';
+
+function DocumentPage() {
+  return (
+    <div>
+      <BillOfLanding></BillOfLanding>
+    </div>
+  );
+}
+
+export default DocumentPage;
