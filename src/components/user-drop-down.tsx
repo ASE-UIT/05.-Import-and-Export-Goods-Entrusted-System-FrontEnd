8467abf957d11b3e191b0fea7f82a66392ae59e2
PhuscBui <duyphucbui522@gmail.com>
2024-12-21 16:07:27 +0700

    Add legal representative management features with schema, hooks, and UI components

--- src/components/user-drop-down.tsx
+++ src/components/user-drop-down.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { LogOut, Settings, User } from "lucide-react";
+import { LogOut, Settings } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import {
   DropdownMenu,
@@ -14,12 +14,25 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 import useAuth from "@/hooks/use-auth";
 import Loader from "@/components/loader";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ErrorType } from "@/types/error.type";
 import { toast } from "@/hooks/use-toast";
+import useCustomer from "@/hooks/use-customer";
+import { Customer, Employee } from "@/types/user.type";
 
 const UserDropDown = () => {
   const { data: user, isLoading } = useAuth.useGetSession();
+  const { useDetailsCustomer } = useCustomer();
+  const { data: customer } = useDetailsCustomer(user?.customerId ?? "");
+  const [currentUser, setCurrentUser] = useState<Employee | Customer>();
+  useEffect(() => {
+    if (user && user.customerId) {
+      setCurrentUser(customer);
+    } else {
+      setCurrentUser(user?.employee);
+    }
+  }, [user, customer]);
+
   const logout = useAuth.useLogout();
   const route = useRouter();
   const [loading, setLoading] = useState(false);
@@ -69,21 +82,21 @@ const UserDropDown = () => {
       <DropdownMenuTrigger className="rounded px-4 py-1 outline-none xl:hover:bg-foreground/10">
         <div className="flex items-center justify-center md:gap-3">
           <div className="invisible w-0 text-right sm:visible sm:w-auto">
-            <div className="text-[14px]">{user?.employee.name}</div>
+            <div className="text-[14px]">{currentUser?.name}</div>
             <div className="text-xs opacity-50">{user?.role.name}</div>
           </div>
           <Avatar>
             <AvatarImage
-              src={`https://api.dicebear.com/9.x/initials/svg?seed=${user?.employee.name}`}
-              alt={user?.employee.name}
+              src={`https://api.dicebear.com/9.x/initials/svg?seed=${currentUser?.name}`}
+              alt={currentUser?.name ?? "User"}
             />
-            <AvatarFallback>{user?.employee.name}</AvatarFallback>
+            <AvatarFallback>{currentUser?.name}</AvatarFallback>
           </Avatar>
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="p-2">
         <DropdownMenuLabel className="text-center">
-          {user?.employee.name}
+          {currentUser?.name}
         </DropdownMenuLabel>
         <DropdownMenuSeparator className="my-1 bg-foreground/20" />
         <Link href="/settings">
