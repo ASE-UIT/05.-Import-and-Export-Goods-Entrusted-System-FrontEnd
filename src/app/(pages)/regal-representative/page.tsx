a54dc3db70a9df29dc796bb188ec95385a789277
PhuscBui <duyphucbui522@gmail.com>
2024-12-21 16:07:27 +0700

    Add legal representative management features with schema, hooks, and UI components

---
+++ src/app/(pages)/regal-representative/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { columns } from "@/app/(pages)/regal-representative/components/columns";
+import { DataTable } from "@/app/(pages)/regal-representative/components/data-table";
+import Loader from "@/components/loader";
+import useLegalRep from "@/hooks/use-legalRep";
+import React, { useState } from "react";
+
+export default function RegalRep() {
+  const { useListLegalRep } = useLegalRep();
+
+  const { data, isLoading, error } = useListLegalRep();
+  console.log(data);
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-full w-full">
+        <Loader />
+      </div>
+    );
+
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div className="flex flex-col p-[24px] w-full">
+      <div className="flex flex-col w-full gap-[20px]">
+        <div className="flex justify-between items-center">
+          <span className="text-3xl font-bold">
+            Legal Representative Management
+          </span>
+        </div>
+        {data && <DataTable columns={columns} data={data.results} />}
+      </div>
+    </div>
+  );
+}
