42d01224ba587a6e93fcfe770585d85f7b16caae
PhuscBui <duyphucbui522@gmail.com>
2024-12-22 17:36:17 +0700

    Add employee management features with hooks, schemas, and dashboard integration

--- src/schema/employee.schema.ts
+++ src/schema/employee.schema.ts
@@ -6,11 +6,39 @@ export const CreateEmployeeBody = z
     email: z.string().email(),
     phone: z.string().min(1),
     address: z.string().min(1),
-    position: z.object({}),
+    position: z.enum([
+      "MANAGER",
+      "ACCOUNTANT",
+      "SALES",
+      "CUSTOMER_SERVICE",
+      "HUMAN_RESOURCES",
+      "DOCUMENTATION",
+    ]),
     dob: z.string(),
-    coefficientSalary: z.number().min(0),
-    baseSalary: z.number().min(0),
+    coefficientSalary: z.coerce.number().min(0).positive(),
+    baseSalary: z.coerce.number().min(0).positive(),
+  })
+  .strict();
+
+export const updateEmployeeBody = z
+  .object({
+    name: z.string().min(1),
+    email: z.string().email(),
+    phone: z.string().min(1),
+    address: z.string().min(1),
+    position: z.enum([
+      "MANAGER",
+      "ACCOUNTANT",
+      "SALES",
+      "CUSTOMER_SERVICE",
+      "HUMAN_RESOURCES",
+      "DOCUMENTATION",
+    ]),
+    dob: z.string(),
+    coefficientSalary: z.coerce.number().min(0).positive(),
+    baseSalary: z.coerce.number().min(0).positive(),
   })
   .strict();
 
 export type CreateEmployeeBodyType = z.infer<typeof CreateEmployeeBody>;
+export type UpdateEmployeeBodyType = z.infer<typeof updateEmployeeBody>;
