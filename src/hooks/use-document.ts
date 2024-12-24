6c3543c01511401a434723d3962ee4bc92028b7d
PhuscBui <duyphucbui522@gmail.com>
2024-12-24 11:52:45 +0700

    Refactor document hooks to improve API integration; add document creation functionality with success and error handling

--- src/hooks/use-document.ts
+++ src/hooks/use-document.ts
@@ -1,11 +1,45 @@
-import documentAction from "@/apis/document/document.api";
+import documentAction from "@/apis/document.api";
+import * as docAction from "@/apis/document/document.api";
+import { toast } from "@/hooks/use-toast";
+import { CreateDocumentType } from "@/schema/document.schema";
+import { ErrorType } from "@/types/error.type";
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 const useDocument = {
   useGetDocument(userId?: string, type?: string) {
     return useQuery({
       queryKey: ["documents"],
-      queryFn: () => documentAction.getDocument(userId, type),
+      queryFn: () => docAction.default.getDocument(userId, type),
+    });
+  },
+
+  useCreateDocument(router: ReturnType<typeof useRouter>) {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (createDocumentBody: CreateDocumentType) =>
+        documentAction.createDocument(createDocumentBody),
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ["document"],
+        });
+        toast({
+          title: "Create success",
+          description: "Document created successfully",
+          duration: 10000,
+        });
+        router.push("/quote-request");
+      },
+      onError: (error: ErrorType) => {
+        console.error("Error during create:", error);
+        toast({
+          title: "Create failed",
+          description: "An error occurred while creating document",
+          variant: "destructive",
+          duration: 5000,
+        });
+        throw error;
+      },
     });
   },
 };
